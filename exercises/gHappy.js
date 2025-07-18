9e57656cc086c52aa0c68de8d97102601e5f7cc9
Chris Vo <christopher.vo.h@gmail.com>
2025-07-18 11:38:51 -0500

    completed gHappy

--- exercises/gHappy.js
+++ exercises/gHappy.js
@@ -1,5 +1,5 @@
-module.exports.run = function(str) {
-/*
+module.exports.run = function (str) {
+	/*
 A lowercase 'g' in a string is "happy" if there is another 'g' immediately to its left or right.
 Return true if all the g's in the given string are happy, otherwise return false.
 
@@ -10,4 +10,18 @@ Return true if all the g's in the given string are happy, otherwise return false
         Write your code below the comment.
 */
 
+	for (let i = 0; i < str.length; i++) {
+		if (str[i] === "g") {
+			// check to see if the 'g' has a neighbor 'g'
+			const hasLeftNeighbor = i > 0 && str[i - 1] === "g";
+			const hasRightNeighbor = i < str.length - 1 && str[i + 1] === "g";
+
+			// if 'g' has no neighbor
+			if (!hasLeftNeighbor && !hasRightNeighbor) {
+				return false;
+			}
+		}
+	}
+
+	return true;
 };
