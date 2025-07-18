10f011c2ec6a6fb364ed7698ff2e6c64fea5cff9
Chris Vo <christopher.vo.h@gmail.com>
2025-07-18 10:47:16 -0500

    completed subSum

--- exercises/subSum.js
+++ exercises/subSum.js
@@ -1,6 +1,5 @@
-
-module.exports.run = function(x, y) {
-/*
+module.exports.run = function (x, y) {
+	/*
 	You are given a 2D array. Using the x and y co-ordinates, find the sum of all the numbers from (0, 0) to (x, y)
 
 	Example:
@@ -11,11 +10,36 @@ module.exports.run = function(x, y) {
 
 	 Write your code below the comment.
 */
-const twoDArray = [[0,  4,  2,  1],
-		   [3, -1,  1,  0],
-		   [0,  0,  2,  3],
-		   [1,  3, -3,  0]];
+	const twoDArray = [
+		[0, 4, 2, 1],
+		[3, -1, 1, 0],
+		[0, 0, 2, 3],
+		[1, 3, -3, 0],
+	];
 
+	let sum = 0;
+
+	// edge case if x or y is negative
+	if (x < 0 || y < 0) {
+		return 0;
+	}
+
+	for (let row = 0; row <= y; row++) {
+		// Make sure this column exists in this row
+		if (row >= twoDArray.length) {
+			break;
+		}
+
+		// Add this number to our sum
+		for (let col = 0; col <= x; col++) {
+			if (col >= twoDArray[row].length) {
+				break;
+			}
+
+			let currentNumber = twoDArray[row][col];
+			sum += currentNumber;
+		}
+	}
+
+	return sum;
 };
-
-
