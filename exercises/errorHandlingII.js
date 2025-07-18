8e553d39a39db9ae0de9575c9160bbbfa5a4d1b1
Chris Vo <christopher.vo.h@gmail.com>
2025-07-18 10:00:03 -0500

    completed errorHandlingII

--- exercises/errorHandlingII.js
+++ exercises/errorHandlingII.js
@@ -1,5 +1,5 @@
-module.exports.run = function(url){
-/*
+module.exports.run = async function (url) {
+	/*
 	You are given a url. Make a GET call with axios using the given url.
 	However, there is something wrong with the url, and it throws an error.
 	This error will have a property 'message'. Properly return the message, adding 'Error: ' to the beginning of the message.
@@ -8,4 +8,14 @@ module.exports.run = function(url){
 
 	Write your code below the comment.
 */
+
+	const axios = require("axios");
+
+	try {
+		await axios.get(url);
+		return "Request succeeded unexpectedly";
+	} catch (error) {
+		const errorMessage = error.message;
+		return `Error: ${errorMessage}`;
+	}
 };
